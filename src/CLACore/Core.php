f309b94646058dc13fe24c9fadb00e0de4b0465c
AppleNulia <32245068+AppleNulia@users.noreply.github.com>
2018-01-20 13:39:57 +0100

    Fix Commands Move

--- src/CLACore/Core.php
+++ src/CLACore/Core.php
@@ -16,11 +16,11 @@ use Commands\Spawn;
 use Commands\Fly;
 
 #Economy
-use Commands\AddMoney;
-use Commands\Money;
-use Commands\SeeMoney;
-use Commands\SetMoney;
-use Commands\TakeMoney;
+use Commands\Economy\AddMoney;
+use Commands\Economy\Money;
+use Commands\Economy\SeeMoney;
+use Commands\Economy\SetMoney;
+use Commands\Economy\TakeMoney;
 
 #Events
 use Events\onRespawnEvent;
