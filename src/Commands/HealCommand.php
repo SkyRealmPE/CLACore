65d1b8c9802c704dc6c1c24a983d4f7289634731
AppleNulia <32245068+AppleNulia@users.noreply.github.com>
2018-01-26 16:30:02 +0100

    Create HealCommand.php

---
+++ src/Commands/HealCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Commands;
+
+use pocketmine\Player;
+use pocketmine\Server;
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginCommand;
+
+use pocketmine\utils\TextFormat as C;
+
+use CLACore\Core;
+
+class Heal extends PluginCommand{
+
+    public function __construct($name, Core $plugin){
+        parent::__construct($name, $plugin);
+        $this->setDescription("Heal your self.");
+        $this->setPermission("core.heal");
+    }
+
+  
+    public function execute(CommandSender $sender, string $commandLabel, array $args) {
+        if(!$sender->hasPermission("core.heal")) {
+            $sender->sendMessage(C::RED . "You are not allow to use '$commandLabel' command.");
+        }
+        if(!$sender instanceof Player) {
+            $sender->sendMessage(C::RED . "Please use '$commandLabel' in game.");
+        }
+        if($sender->hasPermission("core.heal") || $sender->isOp()) {
+            if ($sender instanceof Player) {
+                $sender->sendMessage("§aYour Healed your self");
+                $sender->setFood(20);
+                $sender->setHealth(20);
+        }
+        return true;
+    }
+}
